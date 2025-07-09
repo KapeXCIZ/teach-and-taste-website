3cf8be6fba3e80b34df214e47b45c0fe3b504225
KapeXCIZ <alessio.capecchi18@gmail.com>
2025-07-09 08:46:06 +0200

    Risolto problema che impediva la visualizzazione del robots.txt e sitemap.xml

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export const config = {
     // Match all pathnames except for
     // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
-    matcher: ['/((?!_next|favicon.ico|logo|manifest|images).*)']
+    matcher: ['/((?!_next|favicon.ico|logo|manifest|images|robots|sitemap).*)']
 };
